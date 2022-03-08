6b371581c73f9fba76a21fbe51dd926995c23f79
Kyaw Thu <evanch98@gmail.com>
2022-03-08 20:24:09 +0800

    Added the operate function

--- js-file.js
+++ js-file.js
@@ -19,3 +19,19 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 // -------------------------------- END --------------------------------
+
+// This function will call the appropriate operator functions depending on the parameter opt.
+function operate(opt, num1, num2) {
+    if (opt === '+') {
+        return add(num1, num2);
+    }
+    else if (opt === '-') {
+        return subtract(num1, num2);
+    }
+    else if (opt === '*') {
+        return multiply(num1, num2);
+    }
+    else if (opt === '/') {
+        return divide(num1, num2);
+    }
+}
