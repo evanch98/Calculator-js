74ccaa4c69c4382192ce9698bfaab9d83eadfd22
Kyaw Thu <evanch98@gmail.com>
2022-03-09 23:03:49 +0800

    Added more functionalities to the display function.

--- js-file.js
+++ js-file.js
@@ -13,9 +13,6 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if (num2 === 0) {
-        return "error";
-    }
     return num1 / num2;
 }
 // -------------------------------- END --------------------------------
@@ -32,7 +29,10 @@ function operate(opt, num1, num2) {
         return multiply(num1, num2);
     }
     else if (opt === '/') {
-        return divide(num1, num2);
+        if (num2 == 0) {
+            return 'error';
+        }
+        return divide(num1, num2).toFixed(2);
     }
 }
 
@@ -45,18 +45,64 @@ const buttonZ = document.querySelector(".btnz");
 // Required variables
 let num1;
 let num2;
+let opt;
 let numString = "";
 
 // Funtion to display numbers on the display when a button is pressed.
 function display(evt) {
-    if (numString.length > 0 && evt.id == '0') {
-        numString += evt.id;
+
+    // If the evt.id is a numerical value then numString will concatenate the number
+    // unless it starts with 0.
+    if (isNaN(evt.id) != true) {
+        if (numString[0] != '0'){
+            numString += evt.id;
+        }
     }
-    else if (isNaN(evt.id) != true && evt.id != '0') {
-        numString += evt.id;
-    }
-    else if (evt.id == 'p') {
+
+    // If the evt.id is p, numString will concatenate '.'
+    else if (evt.id === 'p') {
         numString += '.';
+    }
+
+    // If the evt.id is either one of the operations, the variable 'opt' will take the evt.id as a value.
+    // The variable num1 will take the numString as its value.
+    // numString will be empty again to take another value.
+    else if (evt.id === '+' || evt.id === '-' || evt.id === '*' || evt.id === '/') {
+        opt = evt.id;
+        num1 = parseFloat(numString);
+        numString = '';
+    }
+
+    // Clear the display
+    else if (evt.id === 'c') {
+        numString = '';
+    }
+
+    // Remove the last number
+    else if (evt.id === 'b') {
+        numString = numString.replace(numString[numString.length - 1], '');
+    }
+
+    // Make a number negative or positive
+    else if (evt.id === 'pm') {
+        if (numString.length === 0 || numString[0] != '-') {
+            numString = '-' + numString;
+        }
+        else if (numString[0] === '-') {
+            numString = numString.replace(numString[0], '');
+        }
+    }
+
+    // If evt.id is '=', num2 will take the numString as its value.
+    // opreate() function will be invoked.
+    // num1 will take numString, answer, as its value.
+    else if (evt.id === '=') {
+        num2 = parseFloat(numString);
+        if (num1 && num2 || num1 === 0 && num2) {
+            numString = operate(opt, num1, num2);
+            numString = numString.toString();
+            num1 = parseFloat(numString);
+        }
     }
     displayDiv.textContent = numString;
 }
@@ -71,5 +117,11 @@ buttons.forEach((button) => {
 
 buttonZ.addEventListener('click', () => {
     display(buttonZ);
+});
+
+buttonR.forEach((button) => {
+    button.addEventListener('click', () => {
+        display(button);
+    });
 });
 // -------------------------------- END --------------------------------
